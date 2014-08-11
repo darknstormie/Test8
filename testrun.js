f6844c47c712d165dd29c5684be7072cd1815532
TheGoat96 <sheldonbradley@Sheldons-MacBook-Air.local>
2014-08-10 20:18:07 -0400

    New Assignment for Janelle
    Made a change to the file and made a request for an assignment for you

--- testrun.js
+++ testrun.js
@@ -1,3 +1,11 @@
+///////////////////////////////// Alright, this will be our first work project./////////////////////
+
+
+
+///// Assignment(s) for Janelle. Write me a nested if else statement. Meaning an if else statement that is inside of another if else statement.
+
+
+
 if (9 > 10){
 console.log("that's right")
 }
